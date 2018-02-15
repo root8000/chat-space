61f468c2fc1b4387529cea4256ed18f4d492275d
山口のぞみ <yamaguchinozomi@yamaguchinozominoMacBook-Pro.lacal>
2018-02-15 22:51:48 +0900

    Retouch messages controller
    パスの間違いを修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,12 +4,13 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @messages = @group.messages.includes(:user)
+    @group = Group.find(params[:group_id])
   end
 
   def create
-    @messages = @group.messages.new(message_params)
+    @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_message_path(@group), notice: 'メッセージが送信されました'
+      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
